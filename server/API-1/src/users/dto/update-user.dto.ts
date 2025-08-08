ab218318a68128fc8d7d6a9143b07c2c6d9a4993
camunozv <93873199+camunozv@users.noreply.github.com>
2025-08-08 11:15:05 -0500

    Almost there 2

--- server/API-1/src/users/dto/update-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserDto } from "./create-user.dto";
-import { PartialType } from "@nestjs/mapped-types";
-
-// We define here the updated version of the previous type s.t
-// all its fields are optional, making it suitable for update operations
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-
-}
