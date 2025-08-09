9d67a6fd441687b14504879dd1cd38171939c193
camunozv <93873199+camunozv@users.noreply.github.com>
2025-08-09 17:45:24 -0500

    Added deletion, creation, and fetching

--- mobile/task_manager_v1/lib/home_page.dart
+++ mobile/task_manager_v1/lib/home_page.dart
@@ -14,59 +14,80 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _controller = TextEditingController();
 
-  List items = [
-    ["Clean the room", false],
-    ["Learn flutter", false],
-  ];
+  List<dynamic> items = [];
 
   void checkBoxChanged(int index) {
     setState(() {
-      items[index][1] = !items[index][1];
+      items[index]['status'] = !items[index]['status'];
     });
   }
 
-  void addNewTask() {
-    setState(() {
-      items.add([_controller.text, false]);
-      _controller.clear();
-    });
+  Future<void> deleteTask(int index, int id) async {
+
+    var url = Uri.parse(
+      "https://proyecto-tareas-rbqt.onrender.com/proyecto-tareas/task/$id",
+    );
+
+    var response = await http.delete(url);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        items.removeAt(index);
+      });
+    } else {
+      print("No task deleted");
+    }
   }
 
-  void deleteTask(int index) {
-    setState(() {
-      items.removeAt(index);
-    });
-  }
+  Future<void> fetchTasks() async {
+    var url = Uri.parse(
+      "https://proyecto-tareas-rbqt.onrender.com/proyecto-tareas/task",
+    );
 
-
-  fetchTasks() async {
-    var url = Uri.parse("https://proyecto-tareas-dey3.onrender.com/proyecto-tareas/task");
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
       // do something
-      print("Data fetched : ${response.body}");
+      setState(() {
+        items = json.decode(response.body);  
+        print(items);
+      });
+      print("Data fetched");
     } else {
       print("No data found");
     }
-
   }
 
-  Future<void> createTasks() async {
-    var url = Uri.parse("https://proyecto-tareas-dey3.onrender.com/proyecto-tareas/task");
-    var response = await http.post(url, body: {'taskName : "nombre", taskDescription: ""'});
+  // 4. Don't forget to dispose it when done
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
-    if (response.statusCode == 200) {
-      // do something 
-      print("Data fetched : ${response.body}");
+  // Hier müssen wir das Post verwenden
+  Future<void> addNewTask() async {
+    var url = Uri.parse(
+      "https://proyecto-tareas-rbqt.onrender.com/proyecto-tareas/task/",
+    );
+    String userInput = _controller.text;
+
+    final headers = {'Content-Type': 'application/json'};
+    final body = json.encode({'taskName': '$userInput', 'taskText': ''});
+    var response = await http.post(url, headers: headers, body: body);
+
+    print(userInput);
+
+    if (response.statusCode == 201) {
+      setState(() {
+        items.add(json.decode(response.body));
+        _controller.clear();
+      });
     } else {
-      print("No data found");
+      print(response.statusCode);
+      print("No data added");
     }
-
   }
-  
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +95,6 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.deepPurple.shade300,
       appBar: AppBar(
         title: Text("My ToDo APP"),
-        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: fetchTasks)],
         backgroundColor: Colors.deepPurpleAccent,
         foregroundColor: Colors.black,
         centerTitle: true,
@@ -84,10 +104,10 @@ class _HomePageState extends State<HomePage> {
         itemCount: items.length,
         itemBuilder: (BuildContext context, index) {
           return TodoList(
-            taskName: items[index][0],
-            taskState: items[index][1],
+            taskName: items[index]['taskName'],
+            taskState: items[index]['status'],
             onChanged: (value) => checkBoxChanged(index),
-            deleteFunction: (context) => deleteTask(index),
+            deleteFunction: (context) => deleteTask(index, items[index]['id']),
           );
         },
       ),
@@ -115,9 +135,18 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          FloatingActionButton(
-            onPressed: () => addNewTask(),
-            child: Icon(Icons.add),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FloatingActionButton(
+                onPressed: () => addNewTask(),
+                child: Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () => fetchTasks(),
+                child: Icon(Icons.refresh),
+              ),
+            ],
           ),
         ],
       ),
